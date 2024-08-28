b0dd8551d767f1066b13a9864ac6b1740dcd826f
liangshengfeng <liangshengfeng@star-net.cn>
2024-08-28 10:48:29 +0800

    [fix][让lualine显示完整路径没有省略号]

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+require("lualine").setup({
+    sections = {
+        lualine_c = { { 'filename', path = 2, shorten = false } }, -- 禁用缩短
+    },
+    inactive_sections = {
+        lualine_c = { { 'filename', path = 2, shorten = false } }, -- 禁用缩短
+    },
+})
+
