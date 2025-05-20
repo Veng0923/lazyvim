8abafa48556740efcd2c75ee37ca94ba3082191e
liangshengfeng <liangshengfeng@star-net.cn>
2025-05-20 15:50:59 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,12 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-require("lualine").setup({
-    sections = {
-        lualine_c = { { 'filename', path = 1, shorten = false } }, -- 禁用缩短
-    },
-    inactive_sections = {
-        lualine_c = { { 'filename', path = 1, shorten = false } }, -- 禁用缩短
-    },
-})
+-- require("lualine").setup({
+    -- sections = {
+        -- lualine_c = { { 'filename', path = 1, shorten = false } }, -- 禁用缩短
+    -- },
+    -- inactive_sections = {
+        -- lualine_c = { { 'filename', path = 1, shorten = false } }, -- 禁用缩短
+    -- },
+-- })
 
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,5 @@ opt.tabstop = 4 -- Tab 显示为4个空格
 opt.shiftwidth = 4 -- 自动缩进为4格
 vim.g.autoformat = false    -- 自动格式化文件
 vim.g.NERDSpaceDelims = 1   -- 注释分割符添加空格
+
+vim.opt.clipboard = "unnamedplus"
